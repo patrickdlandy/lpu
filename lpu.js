dde4051583a09021855aadcf3697a74e615e4a67
Patrick Landy <patrickdlandy@gmail.com>
2023-07-04 00:41:12 -0400

    Demonstrate working recursive main playlist with absolute paths in root folder.

--- lpu.js
+++ lpu.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
+const musicextensions = [".mp3", ".flac", ".m4a", ".wav"];
+
 
 function folderList() {
     console.log(__dirname);
     const results = fs.readdirSync(__dirname);
-    // console.log(results);
     results.forEach(item => {
         if (fs.lstatSync(path.resolve(__dirname, item)).isDirectory()) {
             console.log(item);
@@ -13,6 +14,32 @@ function folderList() {
     });
 }
 
-folderList();
+//folderList();
 
+function writeLine(filePath, filename) {
+    fs.appendFile(filename, filePath + "\n", function(err) {
+        if (err) throw err;
+        console.log(`Wrote ${filePath} to ${filename}`)
+    });
+}
+
+//writeLine("/home/patrick/Code/lpu/Steely Dan- Do It Again.mp3","test.m3u8");
+
+
+function scanFolder(filePath) {
+    const files = fs.readdirSync(filePath);
+    files.forEach(item => {
+        if (fs.lstatSync(path.resolve(filePath, item)).isDirectory()) {
+            console.log(`directory: ${item}`);
+            scanFolder(path.resolve(filePath, item));
+        } else if (fs.lstatSync(path.resolve(filePath, item)).isFile()) {
+            console.log(`file: ${item}`);
+            if (musicextensions.includes(path.extname(item))) {
+                writeLine(path.resolve(filePath, item),"main.m3u8");
+            }
+        }
+    });
+}
+
+scanFolder(".");
 
